b165afcd23b6c558453dc3b2ffac4103e4e52293
Evgeniy Melnikov <evgeniy.melnikov@pantheon.io>
2020-03-13 17:19:54 +0300

    Updates + search_api_logger + redis config.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -25,6 +25,24 @@ include __DIR__ . "/settings.pantheon.php";
  */
 // $settings['skip_permissions_hardening'] = TRUE;
 
+if (defined('PANTHEON_ENVIRONMENT')) {
+  // Include the Redis services.yml file. Adjust the path if you installed to a contrib or other subdirectory.
+  $settings['container_yamls'][] = 'modules/redis/example.services.yml';
+
+  //phpredis is built into the Pantheon application container.
+  $settings['redis.connection']['interface'] = 'PhpRedis';
+  // These are dynamic variables handled by Pantheon.
+  $settings['redis.connection']['host']      = $_ENV['CACHE_HOST'];
+  $settings['redis.connection']['port']      = $_ENV['CACHE_PORT'];
+  $settings['redis.connection']['password']  = $_ENV['CACHE_PASSWORD'];
+
+  $settings['cache']['default'] = 'cache.backend.redis'; // Use Redis as the default cache.
+  $settings['cache_prefix']['default'] = 'pantheon-redis';
+
+  // Set Redis to not get the cache_form (no performance difference).
+  $settings['cache']['bins']['form']      = 'cache.backend.database';
+}
+
 /**
  * If there is a local settings file, then include it
  */
